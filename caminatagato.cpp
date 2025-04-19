538269b7a2fe2b9fbc8d9745b468a43c35275439
K'in <ururawimc@gmail.com>
2025-04-18 23:04:21 -0600

    Update caminatagato.cpp
    camina -> simple -> también para el fondo

--- caminatagato.cpp
+++ caminatagato.cpp
@@ -1,40 +1,51 @@
+//camina simple
 #include<stdio.h>
 #include<allegro5\allegro.h>
 #include<allegro5\allegro_image.h>
-//#include<allegro5\allegro_font.h>
-//#include<allegro5\allegro_ttf.h>
+
+int fin=0;
+
+ALLEGRO_DISPLAY *disp;
+ALLEGRO_EVENT_QUEUE *eventos;
+ALLEGRO_EVENT evento;
+ALLEGRO_TIMER *tempocar;
+ALLEGRO_BITMAP *gato;
 
 void inicializar()
 {
-    int fin=0;
-    if(al_init()==0)
+    if (!al_init())
     {
-        printf("No se pudo incicializar allegro");
-        fin=1;
+        printf("No se pudo inicializar Allegro\n");
+        fin = 1;
     }
 
-    if(!al_install_keyboard())
+    if (!al_init_image_addon())
     {
-        printf("No se inicializ%C el teclado", 162);
-        fin=1;
+        printf("No se pudieron iniciar las imágenes\n");
+        fin = 1;
     }
-    al_init_image_addon(); //iniciar las imágenes
-    return; 
+
+    if (!al_install_keyboard())
+    {
+        printf("No se pudo inicializar el teclado\n");
+        fin = 1;
+    }
 }
-
 
 typedef struct 
 {
     int sx, sy, sw, sh;
 } cuadros;
-enum orientacua 
+
+enum orientacua //enumerar el tipo de movimientos
 {
-    CDERECHA = 0,
-    CIZQUIERDA = 1,
-    CARRIBA = 2,
-    CABAJO = 3
+    movder = 0, //movimiento a la derecha
+    movizqu = 1, //movimiento a la izquierda
+    movarrib = 2, //movimiento arriba
+    movabaj = 3 //movimiento abajo
 };
-cuadros coordenadas[4][3] = //matriz
+
+cuadros coordenadas[4][3] = //matriz, son las coordenadas de cada cuadro en orden para formar su movimiento
 {
     { {330,  32, 109, 150}, {451, 33, 109, 150}, {580,  30, 109, 150} }, // Derecha
     { {580, 219, 109, 150}, {460, 222, 109, 150}, {331, 216, 109, 150} }, // Izquierda
@@ -43,19 +54,12 @@ cuadros coordenadas[4][3] = //matriz
   
 };
 
-
 int main()
 {
+    int mdere = 0, mizq = 0, marrib = 0, mabaj = 0;
     
     inicializar();
 
-    ALLEGRO_DISPLAY *disp;
-    ALLEGRO_EVENT_QUEUE* eventos;
-    ALLEGRO_EVENT evento;
-    ALLEGRO_BITMAP *gato;
-    ALLEGRO_TIMER *tempocar;
-    
-    
     disp=al_create_display(1000,700); //generar disp
     al_set_window_title (disp, "Meowstellar");//nombre de la ventana
     eventos = al_create_event_queue(); //eventos->crear cola de eventos
@@ -67,12 +71,6 @@ int main()
     al_register_event_source(eventos,al_get_keyboard_event_source());
 
     al_start_timer(tempocar);
-
-    int actual = 0; //cuadro que se esté usando
-    int orienta = CABAJO, x=472, y=312;
-    int fin =0,camina=0, otravez=0;
-    int mdere= 0, mizq=0, marrib=0, mabaj=0;
-    
     gato=al_load_bitmap("carnaran.png");
     if(!gato)
     {
@@ -80,114 +78,109 @@ int main()
         fin=1;
     }
 
-    
+    int actual = 0; //cuadro que se esté usando
+    int orienta = movabaj, x=472, y=312, camina=0;
+
+    al_wait_for_event(eventos, &evento);
+
     while(fin==0)
     {
-
-        ALLEGRO_EVENT evento;
-        al_wait_for_event(eventos, &evento);
-
         switch (evento.type)
         {
             case ALLEGRO_EVENT_DISPLAY_CLOSE:
                 fin = 1;
                 break;
+            //detectar si se presiona la tecla 
+            case ALLEGRO_EVENT_KEY_DOWN:
+                    switch (evento.keyboard.keycode)
+                    {
+                        case 83: 
+                            mdere=1; 
+                            break;
+                        case 82: 
+                            mizq=1; 
+                            break;
+                        case 84: 
+                            marrib=1;
+                            break;
+                        case 85: 
+                            mabaj=1; 
+                            break;
+                    }
+                    break;
+            
+            case ALLEGRO_EVENT_KEY_UP:
+                    switch (evento.keyboard.keycode)
+                    {
+                        case 83: 
+                            mdere=0; 
+                            break;
+                        case 82: 
+                            mizq=0; 
+                            break;
+                        case 84: 
+                            marrib=0;
+                            break;
+                        case 85: 
+                            mabaj=0; 
+                            break;
+                    }
+                    if (!mdere && !mizq && !marrib && !mabaj)
+                        camina = 0;
+                    break;
 
             case ALLEGRO_EVENT_TIMER:
-            if (mdere) 
-            {
-                x += 6;
-                if (x > 950) x = 950;
-                orienta = CDERECHA;
-                camina = 1;
-            }
-            if (mizq) 
-            {
-                x -= 6;
-                if (x < 0) x = 0;
-                orienta = CIZQUIERDA;
-                camina = 1;
-            }
-            if (marrib) {
-                y -= 6;
-                if (y < 0) y = 0;
-                orienta = CARRIBA;
-                camina = 1;
-            }
-            if (mabaj) 
-            {
-                y += 6;
-                if (y > 620) y = 620;
-                orienta = CABAJO;
-                camina = 1;
-            }
+                camina = mdere || mizq || marrib || mabaj;
 
-            if (camina)
-                actual = (actual + 1) % 3;
-            else
+                if(mdere) 
+                { 
+                    if(x+8<1000)
+                    {
+                        x+=8; 
+                        orienta=movder; 
+                    }
+                }
+                if(mizq)  
+                { 
+                    if(x-8>0)
+                    {
+                        x-=8; 
+                        orienta=movizqu; 
+                    }
+                }
+                if(marrib)
+                { 
+                    if(y-8>0)
+                    {
+                        y-=8; 
+                        orienta=movarrib;
+                    } 
+                }
+                if(mabaj) 
+                { 
+                    if(y+8<700)
+                    {
+                        y+=8; 
+                        orienta=movabaj; 
+                    }
+                }
+                if(camina)
+                    actual=(actual+1)%3;
+                else
                 actual = 0;
+                 
+                al_clear_to_color(al_map_rgb(0, 0, 0));//al_draw_scaled_bitmap(fondo, 0, 0, 1000, 700, 0, 0, 1000, 700, 0); por si hay fondo
 
-            otravez = 1;
-            break;
+                cuadros f = coordenadas[orienta][actual];
+                float escala = 0.7;
+                al_draw_scaled_bitmap(gato, f.sx, f.sy, f.sw, f.sh, x, y, f.sw * escala, f.sh * escala, 0);
 
-            case ALLEGRO_EVENT_KEY_DOWN:
-            switch (evento.keyboard.keycode) 
-            {
-                case ALLEGRO_KEY_RIGHT:
-                    mdere = 1;
-                    break;
-                case ALLEGRO_KEY_LEFT:
-                    mizq = 1;
-                    break;
-                case ALLEGRO_KEY_UP:
-                    marrib = 1;
-                    break;
-                case ALLEGRO_KEY_DOWN:
-                    mabaj = 1;
-                    break;
-            }
-            break;
-        
-        case ALLEGRO_EVENT_KEY_UP:
-            switch (evento.keyboard.keycode) {
-                case ALLEGRO_KEY_RIGHT:
-                    mdere = 0;
-                    break;
-                case ALLEGRO_KEY_LEFT:
-                    mizq = 0;
-                    break;
-                case ALLEGRO_KEY_UP:
-                    marrib = 0;
-                    break;
-                case ALLEGRO_KEY_DOWN:
-                    mabaj = 0;
-                    break;
-            }
-            if (!mdere && !mizq&& !marrib && !mabaj)
-                    camina = 0;
-            break;
-        }
-
-        if (otravez && al_is_event_queue_empty(eventos)) 
-        {
-            otravez = 0;
-
-            al_clear_to_color(al_map_rgb(0, 0, 0));
-
-            cuadros f = coordenadas[orienta][actual];
-            float escala = 0.7;
-
-            al_draw_scaled_bitmap(gato, f.sx, f.sy, f.sw, f.sh, x, y, f.sw * escala, f.sh * escala, 0);
-
-            al_flip_display();
+                al_flip_display();
+                break;
         }
     }
-    
     al_destroy_display(disp);
     al_destroy_event_queue(eventos);
     al_destroy_bitmap(gato);
-
     return 0;
 }
-
-
